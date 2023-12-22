9484f0849aae8b0d072ab334a1b676c1a3d0eec1
Robert Tran <roberttran@ad.pakko.com.au>
2023-12-22 13:26:42 +1000

    first commit

---
+++ components/ModelSection.jsx
@@ -0,0 +1,25 @@
+"use client"
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Model } from "./Model";
+import { Preload, PerspectiveCamera,OrbitControls } from "@react-three/drei";
+
+export default function ModelSection() {
+  return (
+    <div className="w-full h-[100vh]">
+      <Canvas>
+        <ambientLight intensity={1.5} />
+        <directionalLight
+          castShadow
+          position={[5, 5, 5]}
+          shadow-mapSize-width={2048} // Increasing shadow map width
+          shadow-mapSize-height={2048} // Increasing shadow map height
+        />{" "}
+        <Suspense>
+            <Model />
+        </Suspense>
+        <Preload all />
+      </Canvas>
+    </div>
+  );
+}
